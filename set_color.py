a20cb08bb10a9fe734dba18e69744e6fa8d1ad48
Lin1225 <a3ie9981@gmail.com>
2020-02-18 20:23:06 +0800

    fix

---
+++ set_color.py
@@ -0,0 +1,26 @@
+import os
+import random as rand
+data_dir=os.listdir("data/") 
+print(data_dir)
+
+color_dict=dict()
+
+f=open("color.txt","w+")
+for line in f:
+    print(line,end='')
+    line_list=line.split(' ')
+
+    color_index=line_list[0]
+    color_information_temp=line_list[1]
+        
+    color_information=color_information_temp[0:-2]
+
+    color_dict[color_index]=color_information
+
+    print("color_index",color_index,"color_information",color_information,"\n")
+
+for index in data_dir:
+    if index not in color_dict:
+        with open('color.txt', 'a') as f:
+            line=str(index)+" "+str(rand.randint(0,255))+","+str(rand.randint(0,255))+","+str(rand.randint(0,255)) + '\n'
+            f.writelines(line)
